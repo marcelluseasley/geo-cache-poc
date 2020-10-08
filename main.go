5c55d1aebe3db7198d7dcfd587289ee613859286
Marcellus Easley <marcellus.easley@x-team.com>
2020-10-08 00:13:03 -0400

    add logic to get coord with variable precision and logic to create time object for each log event

--- main.go
+++ main.go
@@ -11,6 +11,26 @@ import (
 	"time"
 )
 
+// Month constants
+
+var months = map[string]time.Month{
+	"Jan": 1,
+	"Feb": 2, 
+	"Mar": 3,
+	"Apr": 4,
+	"May": 5,
+	"Jun": 6,
+	"Jul": 7,
+	"Aug": 8,
+	"Sep": 9,
+	"Oct": 10,
+	"Nov": 11,
+	"Dec": 12,
+}
+
+
+const Precision = 2
+
 // LogEntry ...
 type LogEntry struct {
 	event time.Time
@@ -51,26 +71,59 @@ func main() {
 
 	for _, eachLine := range text {
 		res := regex.FindAllStringSubmatch(eachLine, -1)
-		fmt.Println(len(res[0]))
-		for i, j := range res[0] {
-			fmt.Println(i, j)
+		
+		// create time object
+		year, err := strconv.Atoi(res[0][4])
+		if err != nil {
+			log.Fatal("invalid year")
+			continue
 		}
-		break
+
+		month := months[res[0][3]]
+
+		day, err := strconv.Atoi(res[0][1])
+		if err != nil {
+			log.Fatal("invalid day")
+			continue
+		}
+
+		hours, err := strconv.Atoi(res[0][5])
+		if err != nil {
+			log.Fatal("invalid hour")
+			continue
+		}
+
+		minutes, err := strconv.Atoi(res[0][6])
+		if err != nil {
+			log.Fatal("invalid minute")
+			continue
+		}
+
+		seconds, err := strconv.Atoi(res[0][7])
+		if err != nil {
+			log.Fatal("invalid seconds")
+			continue
+		}
+		latitude := res[0][10]
+		longitude := res[0][8]
+		t := time.Date(year, month, day, hours, minutes, seconds, 0, time.UTC)
+		fmt.Printf("%s lat=%s long=%s\n", t.Local(), coordPrecisionOfX(latitude, Precision), coordPrecisionOfX(longitude, Precision))
+		
 	}
 
 }
 
 // CoordPrecisionOfX returns a string representation of a geographical coordinate to a specific number of decimal places (truncation, no rounding)
-func CoordPrecisionOfX(f float64, x int) string {
-	s64 := strconv.FormatFloat(f, 'f', -1, 64)
+func coordPrecisionOfX(coord string, x int) string {
+	//s64 := strconv.FormatFloat(f, 'f', -1, 64)
 
-	decIndex := strings.Index(s64, ".")
+	decIndex := strings.Index(coord, ".")
 
 	if decIndex == -1 {
-		return s64
+		return coord
 	}
-	if decIndex+x+1 >= len(s64) {
-		return s64[0:]
+	if decIndex+x+1 >= len(coord) {
+		return coord[0:]
 	}
-	return s64[0 : decIndex+x+1]
+	return coord[0 : decIndex+x+1]
 }
