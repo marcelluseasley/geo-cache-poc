16b020ed5a5bb5d1ccdfc395531e42c09b3cadbd
Marcellus Easley <marcellus.easley@x-team.com>
2020-10-08 01:27:04 -0400

    added command line flags

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -15,7 +16,7 @@ import (
 
 var months = map[string]time.Month{
 	"Jan": 1,
-	"Feb": 2, 
+	"Feb": 2,
 	"Mar": 3,
 	"Apr": 4,
 	"May": 5,
@@ -29,24 +30,33 @@ var months = map[string]time.Month{
 }
 
 
-const Precision = 2
 
 // LogEntry ...
 type LogEntry struct {
-	event time.Time
+	event     time.Time
 	latitude  string
 	longitude string
 }
 
+func (le *LogEntry) coordString() string {
+	return fmt.Sprintf("%s_%s", le.latitude, le.longitude)
+}
+
 func main() {
 
-	//le := LogEntry{}
+	filePtr := flag.String("f", "log.txt", "name of log file")
+	precPtr := flag.Int("p", 2, "precision of latitude/longitude")
+	cachePtr := flag.Int("ttl", 3, "cache TTL")
+	flag.Parse()
+	precision := *precPtr
+	cacheTTL := *cachePtr
+	var newCount int
+	var dupCount int
+	cache := make(map[string]time.Time)
 
-	regex := *regexp.MustCompile(`([0-2][0-9]|(3)[0-1])/(\w{3})/(\d{4}):(\d{2}):(\d{2}):(\d{2}).+geoLongitude=(-?\d+(\.\d+)?)&geoLatitude=(-?\d+(\.\d+)?)`)
-
-	file, err := os.Open("log.txt")
+	file, err := os.Open(*filePtr)
 	if err != nil {
-		log.Fatal("failed to open")
+		log.Fatalf("failed to open: %s", *filePtr)
 	}
 
 	scanner := bufio.NewScanner(file)
@@ -58,20 +68,22 @@ func main() {
 
 	file.Close()
 
-/*
-1 - Day
-3 - Month
-4 - Year
-5 - Hour
-6 - Minute
-7 - Second
-8 - Longitude
-10 - Latitude
-*/
+	/*
+	   regexp match groups
+	   1 - Day
+	   3 - Month
+	   4 - Year
+	   5 - Hour
+	   6 - Minute
+	   7 - Second
+	   8 - Longitude
+	   10 - Latitude
+	*/
+	regex := *regexp.MustCompile(`([0-2][0-9]|(3)[0-1])/(\w{3})/(\d{4}):(\d{2}):(\d{2}):(\d{2}).+geoLongitude=(-?\d+(\.\d+)?)&geoLatitude=(-?\d+(\.\d+)?)`)
 
 	for _, eachLine := range text {
 		res := regex.FindAllStringSubmatch(eachLine, -1)
-		
+
 		// create time object
 		year, err := strconv.Atoi(res[0][4])
 		if err != nil {
@@ -107,13 +119,31 @@ func main() {
 		latitude := res[0][10]
 		longitude := res[0][8]
 		t := time.Date(year, month, day, hours, minutes, seconds, 0, time.UTC)
-		fmt.Printf("%s lat=%s long=%s\n", t.Local(), coordPrecisionOfX(latitude, Precision), coordPrecisionOfX(longitude, Precision))
-		
+		le := LogEntry{event: t, latitude: coordPrecisionOfX(latitude, precision), longitude: coordPrecisionOfX(longitude, precision)}
+
+		// caching logic
+		key := le.coordString()
+		if tStamp, ok := cache[key]; !ok {
+			cache[key] = tStamp
+			newCount++
+		} else {
+			if le.event.Before(tStamp.Add(time.Minute * time.Duration(cacheTTL))) {
+				dupCount++
+				cache[key] = le.event // update cache with new timestamp (restart TTL)?
+			} else {
+				cache[key] = le.event
+				newCount++
+			}
+		}
+
 	}
+
+	fmt.Printf("Cache Hits / New MPX requests (lat/long precision of %d) and TTL: %d minutes\n", precision, cacheTTL)
+	fmt.Printf("New: %d\tDuplicate: %d\n", newCount, dupCount)
 
 }
 
-// CoordPrecisionOfX returns a string representation of a geographical coordinate to a specific number of decimal places (truncation, no rounding)
+// coordPrecisionOfX returns a string representation of a geographical coordinate to a specific number of decimal places (truncation, no rounding)
 func coordPrecisionOfX(coord string, x int) string {
 	//s64 := strconv.FormatFloat(f, 'f', -1, 64)
 
